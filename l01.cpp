44c1108a601dfa9856d5ee5ab915087d31add48d
Tyler Richard <2023trichard@tjhsst.edu>
2022-08-26 09:49:59 -0400

    Testing

--- l01.cpp
+++ l01.cpp
@@ -1,7 +1,23 @@
-#include <stdio>;
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     cout << "This is my first C++ project!" << endl;
     return 0;
 }
+
+class Image {
+public:
+
+    Image(int width, int height, string name) : width(width), height(height), name(name) {}
+
+    void print() {
+        cout << this->width << "\n" << this->height << "\n" << this->name << endl;
+    }
+
+private:
+    int width;
+    int height;
+    string name;
+};
