e117c1d41495f3212a6ed2cd1debe5acf486db5a
Tyler Richard <2023trichard@tjhsst.edu>
2022-08-31 13:35:47 +0000

    Fixed 2D array logic

---
+++ .ipynb_checkpoints/image-checkpoint.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace image
+{
+    class Image
+    {
+    public:
+        Image(std::string name);
+        void output();
+        void write(int x, int y, int r, int g, int b);
+
+    private:
+        int width;
+        int height;
+        std::string name;
+        unsigned int imageData[800][800 * 3];
+    };
+}
